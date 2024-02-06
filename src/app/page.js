d9b3b962fd73b15effafc2a8ba77420db94c19bf
prasad shelar <prasad.s.shelar@gmail.com>
2024-02-06 18:02:11 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Body from "./components/Body";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+
+export default function Home() {
+  return (
+    <main className="h-screen overflow-y-scroll bg-gradient-to-b from-purple-700 to-indigo-900">
+
+      <Header />
+
+      <Body/>
+
+      <Projects/>
+
+      <Skills/>
+
+      <Contact/>
+    </main>
+  );
+}
